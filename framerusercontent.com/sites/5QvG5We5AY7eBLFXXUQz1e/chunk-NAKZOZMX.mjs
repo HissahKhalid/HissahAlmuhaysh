5da6701061e8e27845692bca64af55cae73c35c7
Hissah Khalid <124397723+HissahKhalid@users.noreply.github.com>
2025-07-24 06:40:47 +0300

    Add files via upload

---
+++ framerusercontent.com/sites/5QvG5We5AY7eBLFXXUQz1e/chunk-NAKZOZMX.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"Welcome to the portfolio of Hissah Almuhaysh!",favicon:"https://framerusercontent.com/assets/BkHmBseuoKzekey0ORCkRnbIUwU.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/JGlN0oL1GXd62kJqJsg6vGK8.png",title:"HissahAlmuhaysh"}}export{t as a};
+//# sourceMappingURL=chunk-NAKZOZMX.mjs.map
